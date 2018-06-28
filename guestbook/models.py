d059eb52ac282fb128a1182a1d5bdbb3fdb14fa5
SINAEEE <kimshin1227@naver.com>
2018-06-28 10:34:04 +0900

    guestbook data 입력

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Guestbook(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    message = models.CharField(max_length=2000)
+    regdate = models.DateTimeField(auto_now=True) #들어갈떄 자동으로 현재 시스템시간을 설정하는것
+
+    def __str__(self):
+        return 'Guestbook(%s, %s, %s, %s)' % (self.name, self.password, self.message, str(self.regdate))
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from guestbook.models import Guestbook
 
 # Create your views here.
 
 def index(request):
     return render(request, 'guestbook/index.html')
+
+def add(request):
+    guestbook = Guestbook()
+    guestbook.name = request.POST['name']
+    guestbook.password = request.POST['password']
+    guestbook.message = request.POST['message']
+
+    guestbook.save()
+
+    return HttpResponseRedirect('/guestbook')
+
 
 
